1832954d46337225b682a632e5b17d4024440774
meliber <bigwhitely@gmail.com>
2023-09-29 12:53:54 -0500

    remove leading "/" from "/data" for determining "data" dataset

--- src/zfssdr/main.py
+++ src/zfssdr/main.py
@@ -63,7 +63,7 @@ class Datasets:
         # dataset with name starts with 'dpool' or ends with '/home'
         # is considered as a dataset for data which will not be
         # rollback under system rollback circumstance
-        return dataset.startswith('dpool') or dataset.endswith('/home') or dataset.endswith('/data')
+        return dataset.startswith('dpool') or dataset.endswith('/home') or dataset.endswith('data')
 
     def act(self, action, suffix, filter):
         """
